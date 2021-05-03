a36b9d3bb500d510eb6fd7e1956f21c752902185
Anusha Garg <anusha.garg@ayanworks.com>
2021-05-03 17:40:41 +0530

    "Added test case"

--- test/polygon_did_registry_test.js
+++ test/polygon_did_registry_test.js
@@ -8,8 +8,9 @@ const provider = new ethers.providers.Web3Provider(ganache.provider({ gasLimit: 
 
 // Initializing the build files
 const PolygonDIDRegistryJSON = require('../build/PolygonDidRegistry_PolygonDidRegistry.json');
+// const { it } = require('ethers/wordlists');
 
-let accounts, PolygonDIDRegistryInstance;
+let accounts, PolygonDIDRegistryInstance, PolygonDIDRegistryInstance_test;
 
 // Describing the 1st Test
 // This will setup Ganache with some test address
@@ -40,7 +41,7 @@ describe('Basic Contract Testing', () => {
                 provider.getSigner(accounts[0])
             );
             PolygonDIDRegistryInstance = await PolygonDIDRegistry.deploy();
-
+            PolygonDIDRegistryInstance_test = new ethers.Contract(PolygonDIDRegistryInstance.address, PolygonDIDRegistryJSON.abi, provider.getSigner(accounts[1]));
             assert.ok(PolygonDIDRegistryInstance.address, 'conract address should be present');
         });
 
@@ -102,8 +103,23 @@ describe('Basic Contract Testing', () => {
             );
         });
     });
+    //Describing test 5 to check if test fails if we update with different controller
+    describe('Checking update with wrong controller', async () => {
 
-    // Describing the 5th Test case
+        it('This should not update DID doc ', async () => {
+
+            const did = '0x2f65b747440deaf596892dfc7965040be8b99100';
+            const doc = '{"@context":"https://w3id.org/did/v1","id":"did:polygon:0x2f65b747440deaf596892dfc7965040be8b99100","verificationMethod":[{"id":"did:polygon:0x2f65b747440deaf596892dfc7965040be8b99100","type":"EcdsaSecp256k1VerificationKey2019","controller":"did:polygon:0x2f65b747440deaf596892dfc7965040be8c66666","publicKeyBase58":"NDaEdTguJV39Ns8BZkxQ3XR6GUinZAJfVoyEMkK9fP7XQmpSkT3UsLHB52cFpDqoM6m4Hevtba8pkmjvEG3Ur7ji"}]}';
+            try {
+                assert.ifError(
+                    //sending transaction using different controller
+                    await PolygonDIDRegistryInstance_test.functions.updateDID(did, doc));
+            } catch (error) {
+            }
+        });
+    });
+
+    // Describing the 6th Test case
     describe('Checking the Delete function in Contract', async () => {
 
         it('This should delete the DID doc on Contract and will read and check if the value is now an empty string', async () => {
@@ -127,7 +143,7 @@ describe('Basic Contract Testing', () => {
         });
     });
 
-    // Describing the 6th Test case
+    // Describing the 7th Test case
     describe('Checking the Create function in Contract', async () => {
 
         it('This should store the DID doc on Contract and will read and check if the value stored is not null', async () => {
@@ -154,7 +170,7 @@ describe('Basic Contract Testing', () => {
         });
     });
 
-    // Describing the 7th Test case
+    // Describing the 8th Test case
     describe('Checking the Update function in Contract', async () => {
 
         it('This should update the DID doc on Contract and will read and check if the value stored has changed', async () => {
@@ -182,7 +198,7 @@ describe('Basic Contract Testing', () => {
     });
 
 
-    // Describing the 8th Test case
+    // Describing the 9th Test case
     describe('Checking the Delete function in Contract', async () => {
 
         it('This should delete the DID doc on Contract and will read and check if the value is now not equal to the updated doc', async () => {
