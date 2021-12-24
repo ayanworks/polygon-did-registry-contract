69aa0cc97aaadaab599ce16804d1a7ddd75f218c
anushaayanworks <anusha.garg@ayanworks.com>
2021-12-25 00:31:38 +0530

    "Added transferOwnership"

--- contracts/PolygonDidRegistry.sol
+++ contracts/PolygonDidRegistry.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.8.0;
 
 /**
  *@title PolygonDidRegistry
  *@dev Smart Contract for Polygon DID Method
  */
 contract PolygonDidRegistry {
+    address owner;
     struct PolyDID {
         address controller;
         uint256 created;
@@ -22,20 +23,40 @@ contract PolygonDidRegistry {
     event DidCreated(address id, string doc);
     event DidUpdated(address id, string doc);
     event DidDeleted(address id);
+    event TransferOwnership(address newOwner);
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require( msg.sender == owner, "message sender is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner)public onlyOwner() returns (string memory){
+        if(owner != newOwner){
+            owner = newOwner;
+            emit TransferOwnership(owner);
+            return ("Ownership transffered successfully");
+        }
+        else {
+            return ("New Owner address is equal to original owner address");
+        }
+    }
 
     /**
      *@dev Register a new DID
      *@param _id - Address that will refer the DID doc
      *@param _doc - A string object that holds the DID Doc
-     *@return returns the controller, created and updated time stamp and DID Document
      */
     function createDID(address _id, string memory _doc)
         public
         returns (address controller, uint256 created, uint256 updated, string memory did_doc)
     {
         did[_id].controller = msg.sender;
-        did[_id].created = now;
-        did[_id].updated = now;
+        did[_id].created = block.timestamp;
+        did[_id].updated = block.timestamp;
         did[_id].did_doc = _doc;
         emit DidCreated(_id, _doc);
         return (did[_id].controller, did[_id].created, did[_id].updated, did[_id].did_doc);
@@ -53,14 +74,13 @@ contract PolygonDidRegistry {
      *@dev To Update the DID doc
      *@param _id - Address that refers to the DID doc
      *@param _doc - A String that holds the DID doc
-     *@return returns the controller, created and updated time stamp and DID Document
      */
     function updateDID(address _id, string memory _doc)
         public
         onlyController(_id) returns(address controller, uint256 created, uint256 updated, string memory did_doc)
     {
         did[_id].did_doc = _doc;
-        did[_id].updated = now;
+        did[_id].updated = block.timestamp;
         emit DidUpdated(_id, _doc);
         return (did[_id].controller, did[_id].created, did[_id].updated, did[_id].did_doc);
     }
