99ba20a1ad724928e7eae2d5c545406c73492126
anushaGarg <anusha.garg@ayanworks.com>
2022-09-16 19:05:21 +0530

    Changed the getDIDDocByIndex function

--- contracts/PolygonDidRegistry.sol
+++ contracts/PolygonDidRegistry.sol
@@ -1,9 +1,6 @@
-/**
- *Submitted for verification at polygonscan.com on 2021-12-29
-*/
-
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
+
 // import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /**
@@ -13,7 +10,7 @@ pragma solidity ^0.8.0;
 contract PolygonDidRegistry {
     uint256 totalDIDs;
     address owner;
-    uint256 deletedDIDs;
+    uint256 deletedDID;
     struct PolyDID {
         address controller;
         uint256 created;
@@ -23,11 +20,12 @@ contract PolygonDidRegistry {
 
     modifier onlyController(address _id) {
         require(
-            did[_id].controller == msg.sender, "message sender is not the controller of the DID Doc"
+            polyDIDs[_id].controller == msg.sender,
+            "message sender is not the controller of the DID Doc"
         );
         _;
     }
-    mapping(address => PolyDID) did;
+    mapping(address => PolyDID) polyDIDs;
     mapping(uint256 => address) activeDIDs;
     mapping(address => uint256) deleteDIDs;
     event DidCreated(address id, string doc);
@@ -35,38 +33,40 @@ contract PolygonDidRegistry {
     event DidDeleted(address id);
     event TransferOwnership(address newOwner);
     bool private initialized;
-   
 
     /**
-    *@dev initializes the ownership of contract
-    **/
+     *@dev initializes the ownership of contract
+     **/
 
     function initialize() public {
         require(!initialized, "Contract instance has already been initialized");
         initialized = true;
         owner = msg.sender;
         totalDIDs = 0;
-        deletedDIDs = 0;
+        deletedDID = 0;
     }
 
-    modifier onlyOwner(){
-        require( msg.sender == owner, "message sender is not the owner");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "message sender is not the owner");
         _;
     }
 
     /**
-    *@dev transfer the ownership of contract
-    *@param _newOwner - Address of the new owner to whom the ownership needs to be passed
-    **/
+     *@dev transfer the ownership of contract
+     *@param _newOwner - Address of the new owner to whom the ownership needs to be passed
+     **/
 
-    function transferOwnership(address _newOwner) public onlyOwner() returns (string memory){
-        if(owner != _newOwner){
+    function transferOwnership(address _newOwner)
+        public
+        onlyOwner
+        returns (string memory)
+    {
+        if (owner != _newOwner) {
             owner = _newOwner;
             emit TransferOwnership(owner);
             return ("Ownership transferred successfully");
-        }
-        else {
-            return ("New Owner address is equal to original owner address");
+        } else {
+            return ("Ownership cannot be transferred to the same account");
         }
     }
 
@@ -74,7 +74,7 @@ contract PolygonDidRegistry {
      *@dev Reads contract owner from chain
      */
 
-    function getOwner() public view returns (address _owner){
+    function getOwner() public view returns (address _owner) {
         return owner;
     }
 
@@ -86,17 +86,27 @@ contract PolygonDidRegistry {
 
     function createDID(address _id, string memory _doc)
         public
-        returns (address controller, uint256 created, uint256 updated, string memory did_doc)
+        returns (
+            address controller,
+            uint256 created,
+            uint256 updated,
+            string memory did_doc
+        )
     {
-        did[_id].controller = msg.sender;
-        did[_id].created = block.timestamp;
-        did[_id].updated = block.timestamp;
-        did[_id].did_doc = _doc;
+        polyDIDs[_id].controller = msg.sender;
+        polyDIDs[_id].created = block.timestamp;
+        polyDIDs[_id].updated = block.timestamp;
+        polyDIDs[_id].did_doc = _doc;
         activeDIDs[totalDIDs] = msg.sender;
         deleteDIDs[_id] = totalDIDs;
-        totalDIDs = totalDIDs+1;
+        ++totalDIDs;
         emit DidCreated(_id, _doc);
-        return (did[_id].controller, did[_id].created, did[_id].updated, did[_id].did_doc);
+        return (
+            polyDIDs[_id].controller,
+            polyDIDs[_id].created,
+            polyDIDs[_id].updated,
+            polyDIDs[_id].did_doc
+        );
     }
 
     /**
@@ -104,34 +114,46 @@ contract PolygonDidRegistry {
      *@param _id - Address that refers to the DID doc position
      */
 
-    function getDID(address _id) public view returns (string memory) {
-        return did[_id].did_doc;
+    function getDIDDoc(address _id) public view returns (string memory) {
+        return polyDIDs[_id].did_doc;
     }
 
     /**
      *@dev Reads total number of DIDs from Chain
-    */
+     */
 
-    function getTotalNumberOfDIDs() public onlyOwner() view returns (uint256 _totalDIDs, uint256 _activeDIDs){
-        return (totalDIDs, (totalDIDs-deletedDIDs));
+    function getTotalNumberOfDIDs()
+        public
+        view
+        returns (uint256 _totalDIDs, uint256 _activeDIDs)
+    {
+        return (totalDIDs, (totalDIDs - deletedDID));
     }
 
     /**
      *@dev Reads total number of DIDs deleted from Chain
-    */
+     */
 
-    function getTotalNumberOfDeletedDIDs() public onlyOwner() view returns (uint256 _deletedDID){
+    function getTotalNumberOfDeletedDIDs()
+        public
+        view
+        returns (uint256 _deletedDID)
+    {
         return deletedDID;
     }
 
     /**
      *@dev Reads one DID at a time from Chain based on index
-     *@param _index - Uint256 type variable that refers to the DID position 
-     *@return _did - returns the address associated with DID URI, if the DID is not deleted, else returns empty string.
+     *@param _index - Uint256 type variable that refers to the DID position
+     *@return _did - returns the DID Doc assciated with the index. Returns null if the DID Doc is deleted.
      */
 
-    function getDIDByIndex(uint256 _index) public onlyOwner() view returns (address _did){
-        return allDIDs[_index];
+    function getDIDDOcByIndex(uint256 _index)
+        public
+        view
+        returns (string memory)
+    {
+        return polyDIDs[activeDIDs[_index]].did_doc;
     }
 
     /**
@@ -142,12 +164,23 @@ contract PolygonDidRegistry {
 
     function updateDID(address _id, string memory _doc)
         public
-        onlyController(_id) returns(address controller, uint256 created, uint256 updated, string memory did_doc)
+        onlyController(_id)
+        returns (
+            address controller,
+            uint256 created,
+            uint256 updated,
+            string memory did_doc
+        )
     {
-        did[_id].did_doc = _doc;
-        did[_id].updated = block.timestamp;
+        polyDIDs[_id].did_doc = _doc;
+        polyDIDs[_id].updated = block.timestamp;
         emit DidUpdated(_id, _doc);
-        return (did[_id].controller, did[_id].created, did[_id].updated, did[_id].did_doc);
+        return (
+            polyDIDs[_id].controller,
+            polyDIDs[_id].created,
+            polyDIDs[_id].updated,
+            polyDIDs[_id].did_doc
+        );
     }
 
     /**
@@ -156,8 +189,8 @@ contract PolygonDidRegistry {
      */
 
     function deleteDID(address _id) public onlyController(_id) {
-        deletedDIDs = deletedDIDs +1;
-        delete did[_id];
+        ++deletedDID;
+        delete polyDIDs[_id];
         delete activeDIDs[deleteDIDs[_id]];
         emit DidDeleted(_id);
     }
